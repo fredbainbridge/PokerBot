ea5a302ca91bafa7de3b9810ae2167b633d6f697
Fred Bainbridge <fred@mnscug.org>
2019-10-20 14:36:53 -0500

    updating deploys script

--- PokerBot/deploy.ps1
+++ PokerBot/deploy.ps1
@@ -1,4 +1,5 @@
 $deployPath = "D:\apps\PokerBot"
 Remove-Item -Path $deployPath* -Recurse -Force -Confirm:$false
 dotnet publish -o $deployPath -r win-x64
+Start-Process -FilePath $deployPath\PokerBot.exe -WorkingDirectory $deployPath
 #Copy-Item -Path  .\secrets.json -Destination C:\apps\PokerBot\secrets.json -Force
